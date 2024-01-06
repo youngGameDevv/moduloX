5bca103fe57fdc5c7a5219738e58727854787cc2
youngGameDevv <112010570+youngGameDevv@users.noreply.github.com>
2024-01-06 15:12:32 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -5,24 +5,27 @@ using System;
 
 public class HelloWorld
 {
+    static int mod(int k, int n) {  return ((k %= n) < 0) ? k+n : k;  }
     static  int FindModuloX(int a,int b,int modulo)
     {
-        int x = 0;
-        
         for(int i = 0;i<1000;i++)
         {
-            x+=1;
-            if((a*x-b) % modulo == 0)
-            {
-                 return x;
-            }
+            if(mod(a*i-b,modulo) == 0)
+                return i;
         }
         return 0;
+    }
+    static bool isCorrect(int a,int b,int modulo)
+    {
+        return mod(a-b,modulo) == 0 ? true : false;
     }
 
     public static void Main(string[] args)
     {
-        int mod = 0,a = 0,b = 0;
+        int mod = 0,a = 0,b = 0,selectMode = 1;
+        Console.WriteLine("obliczanie x wybierz 1");
+        Console.WriteLine("sprawdzanie poprawnosci wybierz 2");
+        selectMode = Int32.Parse(Console.ReadLine());
         Console.WriteLine("podaj liczbe stojaca przy x");
         a = Int32.Parse(Console.ReadLine());
         Console.WriteLine("podaj liczbe po =");
@@ -30,6 +33,9 @@ public class HelloWorld
         Console.WriteLine("podaj modulo");
         mod = Int32.Parse(Console.ReadLine());
         
-        Console.WriteLine("wynik = "+FindModuloX(a,b,mod));
+        if(selectMode == 1)
+            Console.WriteLine("wynik = "+FindModuloX(a,b,mod));
+        else
+            Console.WriteLine("wynik = "+isCorrect(a,b,mod));
     }
 }
